110f6376028fa2a85fc2baf3946089566363aa85
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2024-07-09 15:23:08 +0100

    added a delete mutation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-purple-200 h-full px-3 md:px-0">
+  <div class="bg-purple-200 h-[80rem] px-3 md:px-0">
     <h1 class="text-3xl leading-normal pt-7">Todo List</h1>
-    <form @submit.prevent="addTodo" class="space-x-7 inline-block">
+    <form @submit.prevent="addTodo" class="space-x-4 md:space-x-7 inline-block">
       <input
         v-model="newTodoTitle"
         placeholder="New Todo"
@@ -22,24 +22,25 @@
       <li
         v-for="todo in todos"
         :key="todo.id"
-        @click="deleteTodo(todo.id)"
         class="bg-white border-2 flex justify-between items-center p-5 border-gray-300 w-full md:w-[20rem] h-[3rem] mx-auto rounded-xl"
       >
         {{ todo.title }} - {{ todo.completed ? 'Completed' : 'Pending' }}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="30"
-          height="30"
-          viewBox="0 0 30 30"
-          @click="deleteTodo(todo.id)"
-          class="cursor-pointer"
-        >
-          <path
-            d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"
-          ></path>
-        </svg>
+
+        <button @click="deleteTodo(todo.id)">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="30"
+            height="30"
+            viewBox="0 0 30 30"
+            class="cursor-pointer"
+          >
+            <path
+              d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"
+            ></path>
+          </svg>
+        </button>
       </li>
     </ul>
   </div>
@@ -86,49 +87,38 @@ export default {
     const newTodoCompleted = ref(false);
     const todos = ref([]);
 
-    const { result, loading, refetch } = useQuery(GET_TODOS);
+    const { result, loading } = useQuery(GET_TODOS);
 
     onMounted(() => {
       todos.value = result?.value?.todos || [];
     });
 
-    const { mutate: addTodoMutation } = useMutation(ADD_TODO, DELETE_TODO, {
-      update(cache, { data }) {
-        const { addTodo, deleteTodo } = data; // Assuming your mutation returns both added and deleted todo objects
+    const { mutate: addTodoMutation } = useMutation(ADD_TODO, {
+      update(cache, { data: { addTodo } }) {
+        const existingTodos = cache.readQuery({ query: GET_TODOS });
+        cache.writeQuery({
+          query: GET_TODOS,
+          data: {
+            todos: [...existingTodos.todos, addTodo],
+          },
+        });
+      },
+      refetchQueries: [{ query: GET_TODOS }],
+    });
 
+    const { mutate: deleteTodoMutation } = useMutation(DELETE_TODO, {
+      update(cache, { data: { deleteTodo } }) {
         cache.modify({
           fields: {
-            todos(existingTodos = []) {
-              // Handle Adding a Todo
-              if (addTodo) {
-                const newTodoRef = cache.writeFragment({
-                  data: addTodo,
-                  fragment: gql`
-                    fragment NewTodo on Todo {
-                      id
-                      title
-                      completed
-                    }
-                  `,
-                });
-                return [...existingTodos, newTodoRef];
-              }
-
-              // Handle Deleting a Todo
-              if (deleteTodo) {
-                const deletedTodoId = deleteTodo.id;
-                return existingTodos.filter(
-                  (todo) => todo.id !== deletedTodoId
-                );
-              }
-
-              // If no addTodo or deleteTodo data, return existing todos (no change)
-              return existingTodos;
+            todos(existingTodos = [], { readField }) {
+              return existingTodos.filter(
+                (todoRef) => deleteTodo.id !== readField('id', todoRef)
+              );
             },
           },
         });
-        refetch();
       },
+      refetchQueries: [{ query: GET_TODOS }],
     });
 
     const addTodo = async () => {
@@ -151,19 +141,24 @@ export default {
           console.error('Error adding todo:', error.message);
         });
     };
-    // const deleteTodo = async (todoId) => {
-    //   try {
-    //     await addTodoMutation({ variables: { id: todoId } }); // Use addTodoMutation for deletion
-    //     console.log('Todo deleted successfully!');
-    //   } catch (error) {
-    //     console.error('Error deleting todo:', error.message);
-    //   }
-    // };
+    const deleteTodo = async (id) => {
+      const variables = {
+        id,
+      };
+      await deleteTodoMutation(variables)
+        .then(() => {
+          console.log('Todo deleted successfully');
+        })
+        .catch((error) => {
+          console.error('Error deleting todo:', error.message);
+        });
+    };
 
     return {
       todos,
       newTodoTitle,
       newTodoCompleted,
+      deleteTodo,
       addTodo,
       loading,
     };
